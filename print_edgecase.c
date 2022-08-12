f01b1f66171eeb259e34b9ab6b66e6a7a5dbe63b
kalkidanh <kalkidanh89@gmail.com>
2022-08-12 09:12:19 -0700

    edgecase printed

--- print_edgecase.c
+++ print_edgecase.c
@@ -5,7 +5,7 @@
  * print_pointer - Print the value of a pointer variable
  * to the
  * standard output
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
