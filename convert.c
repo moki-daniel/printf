1c71f6143d7930150362b7f75ad8010a4e216c37
kalkidanh <kalkidanh89@gmail.com>
2022-08-12 08:55:57 -0700

    converted

--- convert.c
+++ convert.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
+ * is_printable - Evaluates if a character is printable
  * @c: Char to be evaluated.
  * utilities
  * Return: 1 if c is printable, 0 otherwise
