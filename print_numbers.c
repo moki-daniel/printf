6bb4c4e5dedfa9802ea15e9e35a833ab32fb1ad9
kalkidanh <kalkidanh89@gmail.com>
2022-08-12 09:16:12 -0700

    numbers printed

--- print_numbers.c
+++ print_numbers.c
@@ -11,7 +11,7 @@
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of characters printed
  */
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
